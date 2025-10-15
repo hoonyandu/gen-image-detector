f918ea69cf07f3281a2fd71b4ea2fc30e5e81f0b
hoonyandu <72491706+hoonyandu@users.noreply.github.com>
2025-10-15 14:39:12 +0900

    [Feat] 모델 추론 Gradio 실행 (#2)
    * feat: rename model `example.py` file to `model.py`
    
    * feat: test gradio client
    
    * add: install gradio
    
    * feat: apply gaussian blur test on gradio client
    
    * feat: set model config
    
    * feat: load processor and model
    
    * feat: predict an image with processor and model
    
    * feat: inference an image on gradio app
    
    * style: ruff format

---
+++ src/services/predict.py
@@ -0,0 +1,28 @@
+import torch
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def predict(processor, model, image):
+    print("Preprocessing image...")
+    # Use the processor to prepare the image for the model
+    inputs = processor(images=image, return_tensors="pt").to(device)
+
+    # Perform Inference
+    print("Running inference...")
+    with torch.no_grad():  # Disable gradient calculations for inference
+        outputs = model(**inputs)
+        logits = outputs.logits
+
+    # Interpret the Results
+    # Get the index of the highest logit score -> this is the predicted class ID
+    predicted_class_idx = logits.argmax(-1).item()
+
+    # Use the model's config to map the ID back to the label string ('ai' or 'hum')
+    predicted_label = model.config.id2label[predicted_class_idx]
+
+    # Optional: Get probabilities using softmax
+    probabilities = torch.softmax(logits, dim=-1)
+    predicted_prob = probabilities[0, predicted_class_idx].item()
+
+    return predicted_label, predicted_prob
