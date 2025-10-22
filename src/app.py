a21d4d79631efda1bec0cb1357dcb1faef89ae04
hoonyandu <hoonyandu@users.noreply.github.com>
2025-10-22 01:52:10 +0000

    chore: lint

--- src/app.py
+++ src/app.py
@@ -1,9 +1,12 @@
+import os
+import sys
+
 import gradio as gr
 from PIL import Image, ImageFilter
-import sys, os
+
 sys.path.append(os.path.abspath("."))
 
-from domain.models import load_processor, load_model
+from domain.models import load_model, load_processor
 from services.predict import predict, predict_with_gradcam
 
 processor = load_processor()
